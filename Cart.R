5245a2bf09da3f1c04d4566a5da77dd6e9b8ecba
hearobe <tan.yl1210@gmail.com>
2022-03-18 22:29:29 +0800

    CART and NN with larger testset

--- Cart.R
+++ Cart.R
@@ -103,7 +103,74 @@ confusionMatrix(confusion_matrix_test)
 
 
 
+# ----------------Larger Testset------------------
+
+library(caTools)
+
+train.clean=read.csv("Training_clean.csv")
+test.clean=read.csv("Testing_clean.csv")
+names(test.clean)[names(test.clean) == "severity"] <- "Severity"
+
+train.clean$Severity_num = ifelse(train.clean$Severity == "A&E", 1,
+                                  ifelse(train.clean$Severity == "Polyclinic", 2,
+                                         ifelse(train.clean$Severity == "No_Medical_Attention_Req", 3, 3)))
+test.clean$Severity_num = ifelse(test.clean$Severity == "A&E", 1,
+                                 ifelse(test.clean$Severity == "Polyclinic", 2,
+                                        ifelse(test.clean$Severity == "No_Medical_Attention_Req", 3, 3)))
+
+train.clean = data.frame(train.clean)
+test.clean = data.frame(test.clean)
+
+data = rbind(train.clean, test.clean)
+data = subset(data, select = -c(Severity))
 
 
 
+set.seed(2)
+
+train <- sample.split(Y = data$Severity_num, SplitRatio = 0.7)
+trainset <- subset(data, train == T)
+testset <- subset(data, train == F)
+
+cartMax <- rpart(Severity_num~. , data = trainset, method = 'class', control = rpart.control(minsplit = 2,cp = 0)) 
+
+----------------------------------------------------------------------------------------------
+  #Compute optimal CP on model
+  ## Compute min CVerror + 1SE in maximal tree cart_max.
+  CVerror.cap <- cartMax$cptable[which.min(cartMax$cptable[,"xerror"]), "xerror"] + cartMax$cptable[which.min(cartMax$cptable[,"xerror"]), "xstd"]
+## Find the optimal CP region whose CV error is just below CVerror.cap in maximal tree cart_max.
+i <- 1; j<- 4
+while (cartMax$cptable[i,j] > CVerror.cap) {
+  i <- i + 1
+}
+## Get geometric mean of the two identified CP values in the optimal region if optimal tree has at least one split.
+cp.opt = ifelse(i > 1, sqrt(cartMax$cptable[i,1] * cartMax$cptable[i-1,1]), 1)
+cp.opt
+# --------------------------------- Pruning ----------------------------------------------------
+
+cart<- prune(cartMax,cp=cp.opt)
+
+summary(cart)
+
+# Confusion Matrix on Trainset
+cart.predict.train <- predict(cart, newdata = trainset, type = "class")
+
+library(caret)
+confusion_matrix_train <- table(trainset[,ncol(trainset)], cart.predict.train, deparse.level = 2)
+confusion_matrix_train
+confusionMatrix(confusion_matrix_train)
+# Accuracy : 1
+
+
+
+# Confusion Matrix on Testset
+
+test_temp = subset(testset, select = -c(Severity_num))
+
+
+cart.predict.test <- predict(cart, newdata = test_temp, type = "class")
+
+confusion_matrix_test <- table(testset[,ncol(testset)], cart.predict.test, deparse.level = 2)
+confusion_matrix_test
+confusionMatrix(confusion_matrix_test)
 
--- NeuralNetwork.r
+++ NeuralNetwork.r
@@ -70,3 +70,63 @@ table(data.test$Severity_num,pred.test.m2)
 test.m3 = compute(m3, data.test)
 pred.test.m3 = max.col(data.frame(test.m3$net.result))
 table(data.test$Severity_num,pred.test.m3)
+
+
+# ----------------Cross-Validation and Largest Testset------------------
+
+library(caTools)
+
+train.clean=read.csv("Training_clean.csv")
+test.clean=read.csv("Testing_clean.csv")
+names(test.clean)[names(test.clean) == "severity"] <- "Severity"
+
+
+train.clean$Severity_num = ifelse(train.clean$Severity == "A&E", 1,
+                                  ifelse(train.clean$Severity == "Polyclinic", 2,
+                                         ifelse(train.clean$Severity == "No_Medical_Attention_Req", 3, 3)))
+test.clean$Severity_num = ifelse(test.clean$Severity == "A&E", 1,
+                                 ifelse(test.clean$Severity == "Polyclinic", 2,
+                                        ifelse(test.clean$Severity == "No_Medical_Attention_Req", 3, 3)))
+
+train.clean = data.frame(train.clean)
+test.clean = data.frame(test.clean)
+
+data = rbind(train.clean, test.clean)
+data = subset(data, select = -c(Severity))
+
+
+
+set.seed(2)
+
+train <- sample.split(Y = data$Severity_num, SplitRatio = 0.7)
+trainset <- subset(data, train == T)
+testset <- subset(data, train == F)
+
+data.clean = data.frame(trainset)
+data.clean = data.frame(testset)
+trainset$l1 = ifelse(trainset$Severity_num == 1, 1, 0)
+trainset$l2 = ifelse(trainset$Severity_num == 2, 1, 0)
+trainset$l3 = ifelse(trainset$Severity_num == 3, 1, 0)
+
+testset$l1 = ifelse(testset$Severity_num == 1, 1, 0)
+testset$l2 = ifelse(testset$Severity_num == 2, 1, 0)
+testset$l3 = ifelse(testset$Severity_num == 3, 1, 0)
+
+trainset.noS = subset(trainset, select = -c(Severity_num))
+testset.noS = subset(testset, select = -c(Severity_num))
+
+
+m4 = neuralnet(l1+l2+l3~., data = trainset.noS, hidden=2,
+               act.fct='logistic', linear.output = FALSE)
+
+pred.m4 = max.col(data.frame(m4$net.result))
+table(trainset$Severity_num, pred.m4)
+
+#Test the resulting output
+temp_test <- subset(testset, select = -c(l1, l2, l3, Severity_num))
+head(temp_test)
+m4.test.results <- compute(m4, testset.noS)
+results <- data.frame(actual = testset$dividend, prediction = nn.results$net.result)
+
+pred.m4.test = max.col(data.frame(m4.test.results$net.result))
+table(testset$Severity_num, pred.m4.test)
