5fda4236b9c3088942adc089290a3181f05be855
hearobe <tan.yl1210@gmail.com>
2022-04-02 00:05:52 +0800

    fix cart

--- code/Cart.R
+++ code/Cart.R
@@ -2,6 +2,8 @@ library(data.table)
 library(rpart)
 library(rpart.plot)
 library(caTools)
+library(caret)
+
 
 data.clean=read.csv("data/Training_clean.csv")
 
@@ -74,9 +76,7 @@ scaledVarImpt
 # Confusion Matrix on Trainset
 cart.predict.train <- predict(cart, newdata = data, type = "class")
 
-library(caret)
 confusion_matrix_train <- table(data[,ncol(data)], cart.predict.train, deparse.level = 2)
-confusion_matrix_train
 confusionMatrix(confusion_matrix_train)
 # Accuracy : 1
 
@@ -97,7 +97,6 @@ data1 = subset(data.test, select = -c(severity))
 cart.predict.test <- predict(cart, newdata = data1, type = "class")
 
 confusion_matrix_test <- table(data.test[,ncol(data.test)], cart.predict.test, deparse.level = 2)
-confusion_matrix_test
 confusionMatrix(confusion_matrix_test)
 # Accuracy : 0.9762
 
@@ -105,8 +104,6 @@ confusionMatrix(confusion_matrix_test)
 # ------------------------------------------------
 # ----------------70-30 Testset-------------------
 # ------------------------------------------------
-
-library(caTools)
 
 train.clean=read.csv("data/Training_clean.csv")
 test.clean=read.csv("data/Testing_clean.csv")
@@ -135,18 +132,25 @@ testset <- subset(data, train == F)
 
 cartMax <- rpart(Severity_num~. , data = trainset, method = 'class', control = rpart.control(minsplit = 2,cp = 0)) 
 
-----------------------------------------------------------------------------------------------
-  #Compute optimal CP on model
-  ## Compute min CVerror + 1SE in maximal tree cart_max.
-  CVerror.cap <- cartMax$cptable[which.min(cartMax$cptable[,"xerror"]), "xerror"] + cartMax$cptable[which.min(cartMax$cptable[,"xerror"]), "xstd"]
-## Find the optimal CP region whose CV error is just below CVerror.cap in maximal tree cart_max.
-i <- 1; j<- 4
-while (cartMax$cptable[i,j] > CVerror.cap) {
-  i <- i + 1
-}
-## Get geometric mean of the two identified CP values in the optimal region if optimal tree has at least one split.
-cp.opt = ifelse(i > 1, sqrt(cartMax$cptable[i,1] * cartMax$cptable[i-1,1]), 1)
+#store the cptable
+dt <- data.table(cartMax$cptable)
+
+#number the sequence of the trees 
+dt[, index := 1:nrow(dt)]
+
+#find out minimum index where xerror is min
+min_cp_index <- min(dt[(xerror == min(xerror)), index])
+
+#find the errorcap
+errorcap <- dt[min_cp_index, xerror+xstd]
+
+#find out the optimal index for the cp
+optimal_cp_index <- min(dt[(xerror < errorcap), index])
+
+#Find the geometric mean of the cp for that index and one cp appearing before it 
+cp.opt = sqrt(dt[index ==optimal_cp_index , CP]*dt[index == optimal_cp_index -1, CP])
 cp.opt
+
 # --------------------------------- Pruning ----------------------------------------------------
 
 cart<- prune(cartMax,cp=cp.opt)
@@ -156,9 +160,7 @@ summary(cart)
 # Confusion Matrix on Trainset
 cart.predict.train <- predict(cart, newdata = trainset, type = "class")
 
-library(caret)
 confusion_matrix_train <- table(trainset[,ncol(trainset)], cart.predict.train, deparse.level = 2)
-confusion_matrix_train
 confusionMatrix(confusion_matrix_train)
 # Accuracy : 1
 
@@ -172,6 +174,5 @@ test_temp = subset(testset, select = -c(Severity_num))
 cart.predict.test <- predict(cart, newdata = test_temp, type = "class")
 
 confusion_matrix_test <- table(testset[,ncol(testset)], cart.predict.test, deparse.level = 2)
-confusion_matrix_test
 confusionMatrix(confusion_matrix_test)
 
